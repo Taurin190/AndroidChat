2c36db24e08a7785e893bd89a4c3daad59d83cd7
Koichi Taura <taura.koichi@gmail.com>
2020-12-20 15:42:38 +0900

    add user check on main activity

--- app/src/main/java/com/taurin190/androidchat/presentation/main/MainActivity.kt
+++ app/src/main/java/com/taurin190/androidchat/presentation/main/MainActivity.kt
@@ -2,17 +2,23 @@ package com.taurin190.androidchat.presentation.main
 
 import android.content.Intent
 import android.os.Bundle
-
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.taurin190.androidchat.R
-import com.taurin190.androidchat.databinding.ActivityMainBinding
 import com.taurin190.androidchat.data.model.Room
+import com.taurin190.androidchat.databinding.ActivityMainBinding
 import com.taurin190.androidchat.presentation.chat.ChatActivity
+import com.taurin190.androidchat.presentation.login.LoginActivity
 import com.taurin190.androidchat.presentation.room.RoomCreationActivity
 
 class MainActivity : AppCompatActivity(), MainFragmentListener {
+
     private lateinit var binding:ActivityMainBinding
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,6 +32,17 @@ class MainActivity : AppCompatActivity(), MainFragmentListener {
 
         binding.floatingActionButton.setOnClickListener {
             val intent = Intent(this, RoomCreationActivity::class.java)
+            startActivity(intent)
+        }
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.getCurrentUser()
+        Log.d("DEBUG", "Current User is: " + currentUser.toString())
+        if (currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
