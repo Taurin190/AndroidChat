e2bdcdbe307b3cd1bb26c2d95fefd493a1ab9993
Koichi Taura <taura.koichi@gmail.com>
2020-12-29 18:26:36 +0900

    delete unnecessary import and functions

--- app/src/main/java/com/taurin190/androidchat/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/taurin190/androidchat/presentation/login/LoginActivity.kt
@@ -1,8 +1,6 @@
 package com.taurin190.androidchat.presentation.login
 
-import android.app.Activity
 import android.content.Intent
-import android.os.Build
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
@@ -12,11 +10,8 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.Button
 import android.widget.EditText
-import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -104,16 +99,17 @@ class LoginActivity : AppCompatActivity(), LoginContract.View {
         ).show()
     }
 
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
-    }
-
     override fun showLoginFailed() {
-        TODO("Not yet implemented")
+        binding.loading.visibility = View.GONE
+        Toast.makeText(baseContext, "Authentication failed.",
+                Toast.LENGTH_SHORT).show()
     }
 
     override fun showLoginSuccess(user: FirebaseUser) {
-        TODO("Not yet implemented")
+        binding.loading.visibility = View.GONE
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
 
